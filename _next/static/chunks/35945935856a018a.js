aeaf29cbadad6c159dcfe63dabbec3b7a89b95a0
Bulanklot <Bulanklot@users.noreply.github.com>
2025-10-03 13:05:45 +0000

    deploy: 7e0498b59ac788a0323466fe8f359a522301c38a

---
+++ _next/static/chunks/35945935856a018a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/d6ab8d6565738e60.js",
+  "static/chunks/turbopack-509211f495b14850.js"
+])
---
+++ _next/static/rBY3-6FCjw_9T1Ae4PjLR/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/209597c71eddb6ed.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/top-dom-main//_next/:path+",
+        "destination": "/top-dom-main/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
