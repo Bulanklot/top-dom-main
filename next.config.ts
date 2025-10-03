eb842ad35faaa972d68fc8d3ba9a245e36fdf5be
Bulat Zinatullin <Zinatullinbn@gmail.com>
2025-10-03 15:25:06 +0300

    update next.confug for GitHub Pages export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 import path from 'node:path';
 
-const nextConfig: NextConfig = {
+const isProd= process.env.NODE_ENV === "production";
+const repo = "top-dom-main";
 
+const nextConfig: NextConfig = {
+    output: "export",
+    basePath: isProd ? `/${repo}` : "",
+    assetPrefix: isProd ? `/${repo}/` : "",
+    images: {
+        unoptimized: true,
+    },
 sassOptions: {
     includePaths: [path.join(__dirname, 'source/app/styles')],
     prependData: '@use "bp.scss" as bp;'
