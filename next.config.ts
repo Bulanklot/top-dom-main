c5a4a097b29f7ee459c170c1f7a2878bc08ecd13
Bulat Zinatullin <Zinatullinbn@gmail.com>
2025-10-03 20:54:21 +0300

    test deploying

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 import path from 'node:path';
 
+const isProd= process.env.NODE_ENV === "production";
+const repo = "top-dom-main";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? `/${repo}` : "",
   images: { unoptimized: true },
 sassOptions: {
     includePaths: [path.join(__dirname, 'source/app/styles')],
