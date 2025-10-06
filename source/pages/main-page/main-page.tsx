6c962469fd9efaf15b83667203a8ff64e8b38758
Bulat Zinatullin <Zinatullinbn@gmail.com>
2025-10-06 23:17:12 +0300

    fixed unresolved export

--- source/pages/main-page/main-page.tsx
+++ source/pages/main-page/main-page.tsx
@@ -1,7 +1,6 @@
 import styles from './styles/styles.module.scss'
-import { SwiperSlideUI } from '@/source/pages/main-page/ui/swiper-slide-ui'
 import { SwiperUI } from '@/source/pages/main-page/ui/title-swiper'
-import {GalleryUI} from "@/source/pages/main-page/ui/gallery";
+import { GalleryUI } from '@/source/pages/main-page/ui/gallery'
 export const MainPage = () => {
   return (
     <div className={styles.container}>
