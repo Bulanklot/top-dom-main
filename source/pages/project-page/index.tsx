58d629f7628650ecdf0bb18e1da68eb6f0d82e80
Jedi Knight <jediknightru@gmail.com>
2025-10-17 18:45:00 +0300

    add price details accordion

--- source/pages/project-page/index.tsx
+++ source/pages/project-page/index.tsx
@@ -3,6 +3,7 @@ import styles from "./styles/styles.module.scss";
 import ProjectSlider from "./project-slider";
 import ProjectPrice from "./project-price";
 import projectSlide1 from "@/public/project/slider/project-slide-1.jpg";
+import PriceDetails from "./price-details";
 
 const IMAGES = [projectSlide1.src, projectSlide1.src, projectSlide1.src]
 
@@ -11,6 +12,7 @@ export const ProjectPage: FC = () => {
         <>
             <ProjectSlider images={IMAGES} />
             <ProjectPrice startPrice={19500000} oldPrice={20500000} discountPrice={500000} />
+            <PriceDetails />
 
             <div className={styles.container}>
             </div>
