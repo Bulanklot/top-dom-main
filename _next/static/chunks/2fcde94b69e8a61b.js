143e82b766f43051922534b90cf3f36e703ad2af
JediKnightGIT <JediKnightGIT@users.noreply.github.com>
2025-10-19 18:22:00 +0000

    deploy: f9ddabf71786408a73a384b47ccab42328c247bc

--- _next/static/chunks/2fcde94b69e8a61b.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/8845d7e05d44eee1.js",
-  "static/chunks/turbopack-4f394c721f965399.js"
-])
--- _next/static/chunks/7d16d091360d025c.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/8845d7e05d44eee1.js",
-  "static/chunks/turbopack-6a5a1bc078cf5428.js"
-])
