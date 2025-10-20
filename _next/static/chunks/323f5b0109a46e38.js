378fbce7c2d6cb182b6667effec8c314dca94c78
Bulanklot <Bulanklot@users.noreply.github.com>
2025-10-20 21:01:57 +0000

    deploy: 4242e7b7831640ff736c93a1512867a4eec23906

--- _next/static/chunks/323f5b0109a46e38.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/4e02c4e3eb0955a5.js",
-  "static/chunks/turbopack-9ef8998f29768dec.js"
-])
--- _next/static/chunks/7dd25ac3d940120f.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/4e02c4e3eb0955a5.js",
-  "static/chunks/turbopack-2b5775983a3a7cc3.js"
-])
