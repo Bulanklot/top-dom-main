9abab261666dccf067973699481bf623f86d18a1
Bulat Zinatullin <Zinatullinbn@gmail.com>
2025-10-28 21:37:28 +0300

    maked title slider

--- source/pages/main-page/ui/gallery/index.tsx
+++ source/pages/main-page/ui/gallery/index.tsx
@@ -10,17 +10,37 @@ export const GalleryUI = async () => {
     <div className={styles.container}>
       <h2 className={styles.title}>Подобрали для вас</h2>
       <section className={styles.cardSection}>
-        {/* <ProjectCard />
-        <ProjectCard />
-        <div className={styles.addsBunner}></div> */}
-        {projects.map(project => (
-          <ProjectCard
-            key={project.id}
-            id={project.id}
-            images={project.images}
-            onBasket={false} // или по условию
-          />
-        ))}
+
+          {projects.flatMap((project, index) => {
+              const elements = [
+                  <ProjectCard
+                      key={project.id}
+                      id={project.id}
+                      images={project.images}
+                      onBasket={false}
+                  />
+              ]
+
+              // после 2-го элемента (index === 1)
+              if (index === 1) {
+                  elements.push(
+                      <div key="banner-1" className={styles.addsBunner}>
+                          {/* Твой первый баннер */}
+                      </div>
+                  )
+              }
+
+              // после 4-го элемента (index === 3)
+              if (index === 5) {
+                  elements.push(
+                      <div key="banner-2" className={styles.addsBunner}>
+                          {/* Второй баннер */}
+                      </div>
+                  )
+              }
+
+              return elements
+          })}
       </section>
     </div>
   )
